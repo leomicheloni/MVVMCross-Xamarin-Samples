df738e77a1a726ac40a3fa7b57ce939d93b6f481
Leonardo Micheloni <leomicheloni@hotmail.com>
2017-07-29 19:57:20 +0200

    Initial commit

---
+++ Basic Android/Test.Core/App.cs
@@ -0,0 +1,26 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Core.Services;
+using Test.Core.ViewModels;
+
+namespace Test.Core
+{
+    public class App : MvxApplication
+    {
+        public App()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            Mvx.RegisterType<ICalculation, Calculation>();
+            Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<FirstViewModel>());
+        }
+    }
+}
---
+++ Basic Android/Test.Core/ViewModels/FirstViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Test.Core.ViewModels
+{
+    public class FirstViewModel : MvxViewModel
+    {
+        public ICommand GoCommand
+        {
+            get
+            {
+                return new MvxCommand(() => ShowViewModel<SecondViewModel>());
+            }
+        }
+    }
+
+
+
+}
