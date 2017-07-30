0bd90d117cc4b4ae354f315ef7a414868a6337db
Leonardo Micheloni <leomicheloni@hotmail.com>
2017-07-30 17:33:38 +0200

    Injecting services sample

---
+++ Using Services/Test.Core/Services/IAuthorizationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Core.Services
+{
+    public interface IAuthorizationService
+    {
+        bool AuthorizeUser(string userName, string password);
+
+        bool IsLogged { get; set; }
+    }
+}
---
+++ Using Services/Test.Core/ViewModels/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Test.Core.Services;
+
+namespace Test.Core.ViewModels
+{
+    public class LoginViewModel : MvxViewModel
+    {
+        private string userName;
+        private string password;
+
+        private readonly IMvxNavigationService _navigationService;
+        private IAuthorizationService _authorizationService;
+        public LoginViewModel(IMvxNavigationService navigationService, IAuthorizationService authorizationService)
+        {
+            _navigationService = navigationService;
+            _authorizationService = authorizationService;
+        }
+
+        public IMvxCommand GoCommand => new MvxCommand(async () => await SingIn());
+
+        public bool CanSignIn => !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
+        public string Username
+        {
+            get { return userName; }
+            set
+            {
+                if (base.SetProperty(ref userName, value))
+                    base.RaisePropertyChanged(() => CanSignIn);
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if (base.SetProperty(ref password, value))
+                    base.RaisePropertyChanged(() => CanSignIn);
+            }
+        }
+
+        private async Task SingIn()
+        {
+            await _navigationService.Navigate<SecondViewModel, string>(this.Username);
+        }
+    }
+}
